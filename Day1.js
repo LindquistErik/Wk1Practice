b08a132cca5c55d57282d78f5e270c4f3bf844ea
LindquistErik <43766707+LindquistErik@users.noreply.github.com>
2018-10-01 16:46:07 -0700

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,10 @@
+var my_number = "33"
+console.log(my_number);
+my_number /2
+console.log(my_number/2)
+var someones_number = "13"
+console.log(someones_number)
+var someones_number = "7"
+console.log(someones_number)
+my_number - someones_number
+console.log(my_number - someones_number)
